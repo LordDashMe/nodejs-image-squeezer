5e49419959c07a8032fd09b6e57dd43d8c16b84c
LordDashMe <reyesjoshuaclifford@gmail.com>
2019-05-23 15:54:57 +0800

    fix: remove unused files in the lib folder and added a function to allow overriding of binary file to used by the package.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,6 @@ module.exports = {
 	rules: {
 		'body-leading-blank': [1, 'always'],
 		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 72],
 		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [2, 'never', [
 			'sentence-case', 'start-case', 'pascal-case', 'upper-case'
