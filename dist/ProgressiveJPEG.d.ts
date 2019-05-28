51c0b6151305a6291e364ddca6f06c1437d6b35e
LordDashMe <reyesjoshuaclifford@gmail.com>
2019-05-28 09:40:29 +0800

    fix: remove sync check for image magick version.

--- dist/ProgressiveJPEG.d.ts
+++ dist/ProgressiveJPEG.d.ts
@@ -1,7 +1,5 @@
 import { ImageSqueezerCommon } from './ImageSqueezerCommon';
 export default class ProgressiveJPEG extends ImageSqueezerCommon {
     constructor();
-    load(): void;
-    private verifyRequiredDependencies;
     compress(): Promise<boolean>;
 }
